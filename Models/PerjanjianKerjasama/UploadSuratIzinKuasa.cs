738eb4d21e10856dfadc61b29f5c9064e33ba3db
Naufal Nurfikri Alwan <naufalnurfikrialwan@gmail.com>
2022-03-08 17:20:26 +0700

    penambahan Upload Surat Izin Kuasa

--- Models/PerjanjianKerjasama/UploadSuratIzinKuasa.cs
+++ Models/PerjanjianKerjasama/UploadSuratIzinKuasa.cs
@@ -7,14 +7,30 @@ using System.Web;
 
 namespace Ptpn8.PerjanjianKerjasama.Models
 {
-    [Table("PK_UploadSuratIzinKuasa")]
-    public class UploadSuratIzinKuasa
+    [Table("PK_HDRUploadSuratIzinKuasa")]
+    public class HDRUploadSuratIzinKuasa
     {
-        public Guid UploadSuratIzinKuasaId { get; set; }
-        public Guid InputKerjasamaKebunId { get; set; }
-        public Decimal TahunBuku { get; set; }
-        public String File_SuratKuasa { get; set; }
-        public String File_SuratIzin { get; set; }
-        public String Keterangan { get; set; }
+        public Guid HDRUploadSuratIzinKuasaId { get; set; }
+        public Guid InputKerjasamaKebun_HDRId { get; set; }
+        public Guid KebunId { get; set; }
+        public Guid WilayahId { get; set; }
+        public string NamaKebun { get; set; }
+        public string NomorPermohonan { get; set; }
+        public DateTime TanggalPermohonan { get; set; }
+        public string NamaMitra { get; set; } = "";
+        public string AlamatMitra { get; set; } = "";
+        public string EmailMitra { get; set; } = "";
+        public string NomorTelepon { get; set; } = "";
+        public string UserName { get; set; }
+    }
+    [Table("PK_DTLUploadSuratIzinKuasa")]
+    public class DTLUploadSuratIzinKuasa
+    {
+        public Guid DTLUploadSuratIzinKuasaId { get; set; }
+        public Guid HDRUploadSuratIzinKuasaId { get; set; }
+        public string NamaLampiran { get; set; }
+        public string NomorLampiran { get; set; }
+        public string NamaFile { get; set; }
+        public DateTime TanggalUpload { get; set; }
     }
 }
--- Models/PerjanjianKerjasama/View_UploadSuratIzinKuasa.cs
+++ Models/PerjanjianKerjasama/View_UploadSuratIzinKuasa.cs
@@ -7,15 +7,30 @@ using System.Web;
 
 namespace Ptpn8.PerjanjianKerjasama.Models
 {
-    
-    public class View_UploadSuratIzinKuasa
+
+    public class View_HDRUploadSuratIzinKuasa
     {
-        public Guid UploadSuratIzinKuasaId { get; set; } = Guid.NewGuid();
-        public Guid InputKerjasamaKebunId { get; set; } = Guid.Empty;
-        public Decimal TahunBuku { get; set; } = 0;
-        public String NomorInputView { get; set; } = "";
-        public String File_SuratKuasa { get; set; } = "";
-        public String File_SuratIzin { get; set; } = "";
-        public String Keterangan { get; set; } = "";
+        public Guid HDRUploadSuratIzinKuasaId { get; set; } = Guid.NewGuid();
+        public Guid InputKerjasamaKebun_HDRId { get; set; } = Guid.Empty;
+        public Guid KebunId { get; set; } = Guid.Empty;
+        public Guid WilayahId { get; set; } = Guid.Empty;
+        public string NamaKebun { get; set; } = "";
+        public string NomorPermohonan { get; set; } = "";
+        public DateTime TanggalPermohonan { get; set; } = DateTime.Now;
+        public string NamaMitra { get; set; } = "";
+        public string AlamatMitra { get; set; } = "";
+        public string EmailMitra { get; set; } = "";
+        public string NomorTelepon { get; set; } = "";
+        public string UserName { get; set; } = Ptpn8.Models.CRUDApplicationUser.CRUD.Get1Record(HttpContext.Current.User.Identity.Name).UserName;
+    }
+    public class View_DTLUploadSuratIzinKuasa
+    {
+        public Guid DTLUploadSuratIzinKuasaId { get; set; } = Guid.NewGuid();
+        public Guid HDRUploadSuratIzinKuasaId { get; set; } = Guid.Empty;
+        public string NamaLampiran { get; set; } = "";
+        public string NomorLampiran { get; set; } = "";
+        public string NamaFile { get; set; } = "";
+        public string StatusDokumen { get; set; } = "";
+        public DateTime TanggalUpload { get; set; } = DateTime.Now;
     }
 }
